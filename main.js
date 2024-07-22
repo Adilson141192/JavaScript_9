feb14cc1af617668470bf3cc89380c49bb2a8b5e
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-07-22 07:22:15 -0300

    072204 aula0105

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+function alternarSubmenu(item, mostrar) {
+    const submenu = item.querySelector(".submenu");
 
+    if (submenu) {
+        submenu.style.display = mostrar ? "block" : "none";
+    }
+}
+
+//
 document.querySelectorAll(".cabecalho__lista-item").forEach(item => {
-
+    //
     item.addEventListener("mouseover", () => alternarSubmenu(item, true));
 
-
+    //
     item.addEventListener("mouseout", () => alternarSubmenu (item, false));
 
     item.addEventListener("click", () => {
@@ -14,3 +22,4 @@ document.querySelectorAll(".cabecalho__lista-item").forEach(item => {
         alternarSubmenu(item, !isDisplayer)
     })
 });
+
