556f12d6248c3d5b7843a92272d0928cc572e9ef
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-07-22 08:01:33 -0300

    072224 aula0301

--- main.js
+++ main.js
@@ -1,56 +1,54 @@
 function alternarModal(modalId, abrir) {
-    const modal = document.querySelector(`#${modalId}`);
-  
-    if (abrir) {
-      modal.style.display = "block";
-    } else {
-      modal.style.display = "none";
-    }
+  const modal = document.querySelector(`#${modalId}`);
 
-    document.body.style.overflow = abrir ? "hidden" : "auto";
+  if (abrir) {
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
+
+  document.body.style.overflow = abrir ? "hidden" : "auto";
 }
 
 document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-      alternarModal("ver-modal-inscrito", false);
-
-      document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
-        alternarSubmenu(item, false);
-      });
-    }
+  if (event.key === "Escape") {
+    alternarModal("ver-modal-inscrito", false);
+    document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
+      alternarSubmenu(item, false);
+    });
+  }
 });
 
 function alternarSubmenu(item, mostrar) {
-    const submenu = item.querySelector(".submenu");
+  const submenu = item.querySelector(".submenu");
 
-    if (submenu) {
-        submenu.style.display = mostrar ? "block" : "none";
+  if (submenu) {
+    submenu.style.display = mostrar ? "block" : "none";
 
-        const menuItem = item.querySelector(".cabecalho__lista-item a");
-        menuItem.setAttribute("aria-expanded", mostrar ? true : false);
+    const menuItem = item.querySelector(".cabecalho__lista-item a");
+    menuItem.setAttribute("aria-expanded", mostrar ? true : false);
 
-        const dropdownExpandedIcon = item.querySelector(
-            ".material-symbols-outlined.icone"
-        );
+    const DropdownExpandedIcon = item.querySelector(
+      ".material-symbols-outlined.icone"
+    );
 
-        dropdownExpandedIcon.classList.toggle("active", mostrar);
-    }
+    DropdownExpandedIcon.classList.toggle("active", mostrar);
+  }
 }
 
-//
-document.querySelectorAll(".cabecalho__lista-item").forEach(item => {
-    //
-    item.addEventListener("mouseover", () => alternarSubmenu(item, true));
+//selecionar todos os cabecalho__lista-item
+document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
+  // adicionar um ouvinte mouseover
+  item.addEventListener("mouseover", () => alternarSubmenu(item, true));
 
-    //
-    item.addEventListener("mouseout", () => alternarSubmenu (item, false));
+  // adicionar um ouvinte mouseout
+  item.addEventListener("mouseout", () => alternarSubmenu(item, false));
 
-    item.addEventListener("click", () => {
-        const submenu = item.querySelector('.submenu');
+  item.addEventListener("click", () => {
+    const submenu = item.querySelector(".submenu");
 
-        const isDisplayer = submenu.computedStyleMap.display === "block";
+    const isDisplayed = submenu.style.display === "block";
 
-        alternarSubmenu(item, !isDisplayer)
-    })
+    alternarSubmenu(item, !isDisplayed);
+  });
 });
-
